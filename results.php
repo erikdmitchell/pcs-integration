5d53b4df18b505373537042ed26a82f6bab77c6b
Erik <erikdmitchell@gmail.com>
2017-07-07 10:09:37 -0400

    cleaned up results data

--- results.php
+++ results.php
@@ -47,11 +47,10 @@ echo "<p>$url</p>";
 			
 		// get headers //
 		foreach ($results->find('div b') as $key => $element) :
-			if (!in_array(strtolower($element->plaintext), $fields_to_ignore)) :
-				$headers[]=strtolower($element->plaintext);
-			else :
+			if (in_array(strtolower($element->plaintext), $fields_to_ignore))
 				$keys_to_skip[]=$key;
-			endif;
+				
+			$headers[]=strtolower($element->plaintext);
 		endforeach;
 		
 		// get results //
@@ -59,12 +58,57 @@ echo "<p>$url</p>";
 			$arr=array();
 			
 			foreach ($line->find('span.show') as $key => $span) :
-				if (!in_array($key, $keys_to_skip))
-					$arr[]=trim($span->plaintext);
+				$value=trim(str_replace('&nbsp;', '', $span->plaintext));
+				
+				switch ($headers[$key]) :
+					case 'rider':
+						foreach ($span->find('a') as $a) :
+							foreach ($a->find('span') as $el) :
+								$last=$el->innertext;
+							endforeach;
+							
+							$first=trim(str_replace($last, '', $a->plaintext));
+							$value=$first.' '.$last;
+						endforeach;
+						break;
+					case 'uci':
+						if (empty($value) || $value=='') :
+							$value=0;
+						endif;
+						break;
+					case 'time':
+						// remove chars //
+						$time=$span->innertext;
+						
+						foreach ($span->find('span.time') as $st)
+							$value=trim($st->plaintext);
+						break;
+				endswitch;
+				
+				$arr[]=$value;
 			endforeach;
 			
 			$race_results[]=$arr;
 		endforeach;
+
+		// remove unused fields from headers //
+		foreach ($headers as $key => $header) :
+			if (in_array($header, $fields_to_ignore))
+				unset($headers[$key]);
+		endforeach;
+
+		$headers=array_values($headers);
+
+		// remove unused fields from results //
+		foreach ($race_results as $k => $race_result) :
+			foreach ($race_result as $key => $value) :
+				if (in_array($key, $keys_to_skip))
+					unset($race_result[$key]);
+			endforeach;
+			
+			$race_results[$k]=array_values($race_result);
+		endforeach;
+		
 echo '<pre>';
 print_r($headers);
 print_r($race_results);
